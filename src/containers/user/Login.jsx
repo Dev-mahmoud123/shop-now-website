397b65e68b9ada92ff31e4953c52422a1339e090
Mahmoud Awad.M <Dev-Mahmoud123>
2023-03-03 10:49:08 +0200

    make login logic

--- src/containers/user/Login.jsx
+++ src/containers/user/Login.jsx
@@ -1,36 +1,109 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import "./login.scss";
 
 function Login() {
+  //
+  const [rememberMe, setRememberMe] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  //
+  const handleRemember = (e) => {
+    setRememberMe(e.target.checked);
+  };
+  //
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    console.log(e.target.value);
+  };
+  //
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        '/api/login',
+        {
+          email,
+          password,
+        },
+        {
+          Headers: {
+            lang: "en",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response.data.data);
+      localStorage.setItem("token", response.data.data.token);
+      window.location.to = "/";
+    } catch (error) {
+      setError("Invalid email or password ");
+    }
+  };
+  //
   return (
     <section className="login">
-      <div className="login_container">
+      <article className="login_container">
         <h3>Login</h3>
-        <form>
-          <div className="email-wrapper">
-            <input id="email" type="email" />
+        <form onSubmit={handleSubmit}>
+          <div className="input-box">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              tabIndex="1"
+              value={email}
+              onChange={handleEmail}
+            />
             <label htmlFor="email">Email</label>
           </div>
-          <div className="password-wrapper">
-            <input id="password" type="password" />
+          <div className="input-box">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              tabIndex="2"
+              value={password}
+              onChange={handlePassword}
+            />
             <label htmlFor="password">Password</label>
           </div>
           <div className="options">
             <div className="remember-me">
-              <input type="checkbox" value="remember" id="remember" />
+              <input
+                type="checkbox"
+                name="remember"
+                id="remember"
+                tabIndex="3"
+                checked={rememberMe}
+                onChange={handleRemember}
+              />
               <label htmlFor="remember">Remember me</label>
             </div>
             <div className="forget-password">
               <button>Forget Password ?</button>
             </div>
           </div>
-          <button className="btn-login">Login</button>
+          {error && <p>{error}</p>}
+          <button
+            className="btn-login"
+            type="submit"
+            title="Login"
+            tabIndex="4"
+          >
+            Login
+          </button>
           <p>
             Don't have an account?
             <a href="/login">Register</a>
           </p>
         </form>
-      </div>
+      </article>
     </section>
   );
 }
